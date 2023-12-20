0003266156648e249b545ef10fa72716667a64ac
Leandro Cunha <leandroscunha@gmail.com>
2023-12-21 00:52:45 +0100

    feat: enhancement CSS for head and main nav

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,20 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <header>
+        <h1>Bianca Zanghi</h1>
+        <nav>
+          <a href="#" className="navitem">Casais</a>
+          <a href="#" className="navitem">Famílias</a>
+          <a href="#" className="navitem">Pessoais</a>
+          <a href="#" className="navitem">Sobre</a>
+          <a href="#" className="navitem">Contato</a>
+        </nav>
+      </header>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
