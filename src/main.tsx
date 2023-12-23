5765bb7f2ea923008e968ca7ed9dd869a286c66d
Leandro Cunha <leandroscunha@gmail.com>
2023-12-23 22:57:49 +0100

    feat: add react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import App from './App.tsx'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/casais",
+    element: <Gallery collection="couples" />
+  },
+  {
+    path: "/familias",
+    element: <Gallery collection="couples" />
+  },
+  {
+    path: "/pessoas",
+    element: <Gallery collection="couples" />
+  },
+  {
+    path: "/sobre",
+    element: <About />
+  },
+  {
+    path: "/contato",
+    element: <Contact />
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
